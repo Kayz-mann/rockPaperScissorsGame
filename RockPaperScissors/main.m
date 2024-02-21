26a5df6f176bd503582b87d7b4a85ea863f20aef
Kayode <kayodefredrickbalogun@gmail.com>
2024-02-21 17:02:31 +0100

    Initial Commit

---
+++ RockPaperScissors/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RockPaperScissors
+//
+//  Created by Balogun Kayode on 21/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
